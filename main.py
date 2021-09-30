6b3c875494cd4eb8b2ccf90b5ffc1f5c94962538
cdhuynh <56008239+cdhuynh@users.noreply.github.com>
2021-09-29 22:46:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 import tweepy
+import os
 
-auth = tweepy.OAuthHandler("consumer key", "consumer secret")
-auth.set_access_token("access key", "access key secret")
+from google.cloud import language
 
-api = tweepy.API(auth)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"KEY_PATH"
 
-# Based on the tweepy getting started program.
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.user.screen_name)
+# This is the example provided in the natural language API documentation:
+client = language.LanguageServiceClient()
+text = ""
+document = language.Document(content = text, type_= language.Document.Type.PLAIN_TEXT)
 
-print ("----Test---")
-# Simple test program based on specific user 'NASA'.
-user = api.get_user('NASA')
-screen_name = "NASA"
-nasa_ID = user.id_str
-print("NASA's ID: " + nasa_ID)
-nasa_count = 5
+sentiment = client.analyze_sentiment(request={'document' : document}).document_sentiment
 
-# Retrieve the last 5 recent tweets from NASA, and the username.
-nasa_status = api.user_timeline(screen_name, count = nasa_count)
-print(user.screen_name)
-print(user.followers_count)
-for test_status in nasa_status:
-    print(test_status.id, test_status.text)
-    print("Location: " + test_status.user.location, end = "\n\n")
+print("Text: {}".format(text))
+print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
 
-api.create_favorite(1441567735448686592)
-
-user2 = api.get_user("cdh22222")
-api.update_profile(description = "Just a test")
-
-if user.followers_count != 0:
-    api.update_profile(description = "Just a new description " + str(user.followers_count))
-
-# Testing a streamListener
-print("#Testing a StreamListener ---------------")
+# This is the next step to explore the npl api.
 
 
-class StreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        print(status.text)
 
+client = language.LanguageServiceClient()
+text = """The Mona Lisa is a magnificent piece of work! It was created by Leonardo Da Vinci, an artist from Italy."""
+document = language.Document(content = text, type_= language.Document.Type.PLAIN_TEXT)
 
-myStreamListener = StreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
-myStream.filter(follow=["11348282"])
+entity_examples = client.analyze_entities(document=document, encoding_type='UTF32')
+print("Notable figure: {0}".format(entity_examples))
+
+client = language.LanguageServiceClient()
+text = """What would a pie be classified as in this API? In addition, would it detect the number of pie is repeated, such as pie, pie, pie,
+or would it return nothing in this field? Google Cloud Storage was something I wanted to use but could not get to work within
+this program. Here is a test to see if it can detect the word 'Rome'. Interesting to note how the word 'program' trends on the negative
+sentiment scale rating. """
+document = language.Document(content = text, type_= language.Document.Type.PLAIN_TEXT)
+
+entity_sentiment = client.analyze_entity_sentiment(document=document, encoding_type="UTF32")
+print("Combining the two: {0}".format(entity_sentiment))
+
 
